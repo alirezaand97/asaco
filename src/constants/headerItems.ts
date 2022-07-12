1715af3e2479df7141a3ba21366c53775f3e6478
alirezaand97 <alirezaand97@gmail.com>
2022-07-12 13:56:28 +0430

    change server json data

--- src/constants/headerItems.ts
+++ src/constants/headerItems.ts
@@ -2,10 +2,10 @@ import routeNames from "routers/routeNames";
 import config from "./config";
 const headerItems = [
   { title: "home", url: routeNames.home },
-  { title: "electronics", url: config.categoryUrl + "electronics" },
-  { title: "men's clothing", url: config.categoryUrl + "men's clothing" },
-  { title: "women's clothing", url: config.categoryUrl + "women's clothing" },
-  { title: "jewelery", url: config.categoryUrl + "jewelery" },
+  { title: "mobile", url: config.categoryUrl + "mobile" },
+  { title: "laptop", url: config.categoryUrl + "laptop" },
+  { title: "watch", url: config.categoryUrl + "watch" },
+  { title: "game", url: config.categoryUrl + "game" },
 ];
 
 export default headerItems;
--- src/routers/routeNames.ts
+++ src/routers/routeNames.ts
@@ -1,6 +1,7 @@
 const routeNames = {
   home: "/",
   login: "/login",
+  product: "/product/:id",
 };
 
 export default routeNames;
